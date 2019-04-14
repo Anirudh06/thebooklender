b19fa32ecc0854029cf6840650e970b3bbfe89b9
Anirudh06 <paramprashant23@gmail.com>
2019-04-15 02:25:58 +0530

    new2

--- thebooklender/src/main/java/thebooklender/ooad/database/CaroselDatabase.java
+++ thebooklender/src/main/java/thebooklender/ooad/database/CaroselDatabase.java
@@ -19,9 +19,9 @@ public class CaroselDatabase {
     try {
       Class.forName("com.mysql.cj.jdbc.Driver");
       connection=DriverManager.getConnection(
-          DatabaseConfiguration.dataBaseUrl,
-          DatabaseConfiguration.userName,
-          DatabaseConfiguration.password);
+          "jdbc:mysql://localhost:3306/booklender?useSSL=false",
+					"anirudh",
+					"Pr@shan1");
     }catch(Exception e) {
       System.out.println("Exception while establishing connection");
       System.out.println(e);
